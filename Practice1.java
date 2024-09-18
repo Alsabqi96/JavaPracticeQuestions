2ff11a781a796bffb2d909880e39438543a4dcde
abdulmajeed <abdullmaged98916437>
2024-09-18 11:33:25 +0400

    Initial Commit

--- Practice1.java
+++ Practice1.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Practice1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for the range
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number: ");
+        int end = scanner.nextInt();
+
         int sum = 0;
-        for (int i = 1; i <= 50; i++) {
+
+        // Loop through the range and calculate the sum of even numbers
+        for (int i = start; i <= end; i++) {
             if (i % 2 == 0) {
                 sum += i;
             }
         }
-        System.out.println("The sum of all even numbers between 1 and 50 is: " + sum);
+
+        // Output the result
+        System.out.println("The sum of all even numbers between " + start + " and " + end + " is: " + sum);
     }
 }
--- Practice4.java
+++ Practice4.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class Practice4 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = {12, 45, 67, 23, 89, 34, 90,200, 11};
+        // Ask the user how many numbers they want to enter
+        System.out.print("Enter the number of elements you want in the array: ");
+        int n = scanner.nextInt();
+
+        // Create an array of size 'n'
+        int[] numbers = new int[n];
+
+        // Get the numbers from the user
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        // Find the maximum value
         int max = numbers[0];
-
-        for (int i=1 ; i <numbers.length; i++){
+        for (int i = 1; i < numbers.length; i++){
             if (numbers[i] > max){
                 max = numbers[i];
-
             }
         }
+
+        // Print the maximum value
         System.out.println("The maximum value in the array is: " + max);
     }
 }
+
 
